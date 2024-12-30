b6bfa0701b3c9488caec29393d5e0c9bc2ae0133
Wout Neirynck <wout@monkey-projects.be>
2024-12-30 15:13:45 +0100

    Restore uberjar artifact for images

--- .monkeyci/build.clj
+++ .monkeyci/build.clj
@@ -2,19 +2,23 @@
   (:require [monkey.ci.build.v2 :as m]
             [monkey.ci.plugin.kaniko :as pk]))
 
+(def uberjar-artifact
+  (m/artifact "uberjar"
+              "target/network-tester.jar"))
+
 (def uberjar
   (-> (m/container-job "uberjar")
       (m/image "docker.io/clojure:tools-deps-bullseye-slim")
       (m/script ["clj -X:uberjar"])
-      (m/save-artifacts (m/artifact "uberjar"
-                                    "target/network-tester.jar"))))
+      (m/save-artifacts uberjar-artifact)))
 
 (def image
   (pk/multi-platform-image-job
    {:target-img "fra.ocir.io/frjdhmocn5qi/network-tester:latest"
     :archs [:arm :amd]
     :container-opts
-    {:dependencies ["uberjar"]}}))
+    {:dependencies ["uberjar"]
+     :restore-artifacts [uberjar-artifact]}}))
 
 [uberjar
  image]
